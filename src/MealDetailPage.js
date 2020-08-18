3ffcc00cd813af473eeb5a798cc2d3ad6b2711f8
Chooklii <=benjaminzenth@icloud.com>
2020-08-18 17:08:54 +0200

    Detail and Edit Meal Page check

--- src/MealDetailPage.js
+++ src/MealDetailPage.js
@@ -2,6 +2,8 @@ import React from "react"
 import axios from "axios";
 import constants from "../config.js"
 import {getDateFromTimestamp} from "./helper"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit, faSave, faBackspace, faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons'
 
 class MealDetailPage extends React.Component{
     constructor(props){
@@ -9,15 +11,22 @@ class MealDetailPage extends React.Component{
         this.state = {
             data: [],
             loaded: false,
+            edit: false,
             id: 0
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleInputChangeTextField = this.handleInputChangeTextField.bind(this);
+        this.removeChange = this.removeChange.bind(this);
+        this.navigateBack = this.navigateBack.bind(this);
     }
 
     componentDidMount(){
         const mealId = this.getQueryStringValue("id")
-        this.setState({id: mealId})
+        const previousPage = this.getQueryStringValue("last")
+        this.setState({
+            id: mealId, 
+            lastPage:previousPage})
         this.loadData(mealId)
     }
 
@@ -28,6 +37,8 @@ class MealDetailPage extends React.Component{
                 this.setState({
                     data: data,
                     loaded: true,
+                    name: data.name,
+                    description: data.description,
                     januar: data.januar === 1,
                     februar: data.februar === 1,
                     march: data.march === 1,
@@ -59,11 +70,11 @@ class MealDetailPage extends React.Component{
     }
 
     updateMenu(){
-        const {data, januar, februar, march, april, mai, juni, july, august, september, october, november, december} = this.state
+        const {id, description, name, januar, februar, march, april, mai, juni, july, august, september, october, november, december} = this.state
         const body = {
-            name:data.name,
-            description: data.description,
-            id: data.id,
+            name:name,
+            description: description,
+            id: id,
             januar: januar,
             februar: februar,
             march: march,
@@ -78,7 +89,7 @@ class MealDetailPage extends React.Component{
             december: december
         }
         axios.post("http://"+ constants.IP_ADRESS + "/updateMeal", body).then((response) => {
-            this.loadData(data.id)
+            this.loadData(id)
         })
     }
 
@@ -95,14 +106,60 @@ class MealDetailPage extends React.Component{
         })
     }
 
+    handleInputChangeTextField(event){
+        const target = event.target
+        const value = target.value
+        const name = target.name
+        this.setState({
+            [name]: value
+        })
+    }
+
+    removeChange(){
+        this.setState({
+            name: this.state.data.name,
+            description: this.state.data.description,
+            edit: false
+        })
+    }
+
+    navigateBack(){
+        const {lastPage} = this.state
+        const origin = window.location.origin
+        window.location.href = origin + "/"
+    }
+
     render(){
-        const {data, loaded, januar, februar, march, april, mai, juni, july, august, september, october, november, december} = this.state
+        const {data, name, description, loaded, januar, februar, march, april, mai, juni, july, august, september, october, november, december} = this.state
         if(loaded && data){
         return(
             <div id="main">
-                <div id="headingDetailPage">{data.name}</div>
-                <div id="descriptionDetailPage">{data.description}</div>
+                {!this.state.edit &&
+                <div>
+                    <div id="headingBoxDetailPage">
+                    <button id="navigateBackButton" type="button" onClick={this.navigateBack}><FontAwesomeIcon icon={faLongArrowAltLeft} /></button>
+                        <button id="editButton" type="button" onClick={() => this.setState({edit: true})}><FontAwesomeIcon icon={faEdit} /></button>
+                    </div>
 
+                    <div id="headingDetailPage">{name}</div>
+                    <div id="descriptionDetailPage">{description}</div>
+                </div>
+                }
+
+                {this.state.edit &&
+                <div>
+                    <div>
+                        <input id="nameInputField" onChange={this.handleInputChangeTextField} type="text" name="name" defaultValue={name}/>
+                        <button onClick={this.removeChange} id="removeChangeButton" type="button"><FontAwesomeIcon icon={faBackspace} /></button>
+
+                    </div>
+
+                    <div>
+                        <input id="descriptionInputField" onChange={this.handleInputChangeTextField} type="text" name="description" defaultValue={description}/>
+                        <button id="saveChangeButton" type="button" onClick={() => this.setState({edit: false})}><FontAwesomeIcon icon={faSave} /></button>
+                    </div>
+                </div>
+                }
 
                 <div id="informationDetailPage">
                 <div id="amountDetailPage">{data.amount}x zubereitet</div>
@@ -114,27 +171,27 @@ class MealDetailPage extends React.Component{
 
                 <div id="allMonths">
                 <div id="firstSix">
-                <div> <input onChange={this.handleInputChange} name="januar" type="checkbox" checked={januar}/> Januar </div>
-                <div> <input onChange={this.handleInputChange} name="februar" type="checkbox" checked={februar}/> Febrar </div>
-                <div> <input onChange={this.handleInputChange} name="march" type="checkbox" checked={march}/> März </div>
-                <div> <input onChange={this.handleInputChange} name="april" type="checkbox" checked={april}/> April </div>
-                <div> <input onChange={this.handleInputChange} name="mai" type="checkbox" checked={mai}/> Mai </div>
-                <div> <input onChange={this.handleInputChange} name="juni" type="checkbox" checked={juni}/> Juni </div>
+                <div className="detailPageCheckbox"> <input onChange={this.handleInputChange} name="januar" type="checkbox" checked={januar}/> Januar </div>
+                <div className="detailPageCheckbox"> <input onChange={this.handleInputChange} name="februar" type="checkbox" checked={februar}/> Februar </div>
+                <div className="detailPageCheckbox"> <input onChange={this.handleInputChange} name="march" type="checkbox" checked={march}/> März </div>
+                <div className="detailPageCheckbox"> <input onChange={this.handleInputChange} name="april" type="checkbox" checked={april}/> April </div>
+                <div className="detailPageCheckbox"> <input onChange={this.handleInputChange} name="mai" type="checkbox" checked={mai}/> Mai </div>
+                <div className="detailPageCheckbox"> <input onChange={this.handleInputChange} name="juni" type="checkbox" checked={juni}/> Juni </div>
 
-                <button onClick={() => this.eatMenu()}>Menü essen</button>
+                <button id="eatButtonDetailPage" type="button" onClick={() => this.eatMenu()}>Menü essen</button>
 
 
                 </div>
 
                 <div id="lastSix">
-                <div> <input onChange={this.handleInputChange} name="july" type="checkbox" checked={july}/> Juli </div>
-                <div> <input onChange={this.handleInputChange} name="august" type="checkbox" checked={august}/> August </div>
-                <div> <input onChange={this.handleInputChange} name="september" type="checkbox" checked={september}/> September </div>
-                <div> <input onChange={this.handleInputChange} name="october" type="checkbox" checked={october}/> Oktober </div>
-                <div> <input onChange={this.handleInputChange} name="november" type="checkbox" checked={november}/> November </div>
-                <div> <input onChange={this.handleInputChange} name="december" type="checkbox" checked={december}/> Dezember </div>
+                <div className="detailPageCheckbox"> <input onChange={this.handleInputChange} name="july" type="checkbox" checked={july}/> Juli </div>
+                <div className="detailPageCheckbox"> <input onChange={this.handleInputChange} name="august" type="checkbox" checked={august}/> August </div>
+                <div className="detailPageCheckbox"> <input onChange={this.handleInputChange} name="september" type="checkbox" checked={september}/> September </div>
+                <div className="detailPageCheckbox"> <input onChange={this.handleInputChange} name="october" type="checkbox" checked={october}/> Oktober </div>
+                <div className="detailPageCheckbox"> <input onChange={this.handleInputChange} name="november" type="checkbox" checked={november}/> November </div>
+                <div className="detailPageCheckbox"> <input onChange={this.handleInputChange} name="december" type="checkbox" checked={december}/> Dezember </div>
 
-                <button onClick={() => this.updateMenu()}>Änderung speichern</button>
+                <button id="updateButtonDetailPage" type="button" onClick={() => this.updateMenu()}>speichern</button>
 
 
                 </div>
